4369ace3b0e59e261cc5afa84762876383ff7f62
baselelsrogy <baselhany88@gmail.com>
2026-05-30 02:26:48 +0300

    add logo and switch theme to header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import Logo from './Logo';
+import ThemeSwitcher from './ThemeSwitcher';
+
+export default function Header() {
+  return (
+    <header className="mb-6 bg-white shadow dark:bg-gray-800 md:mb-12 transition-colors">
+      <div className="container mx-auto px-5 md:px-0">
+        <div className="flex h-20 items-center justify-between">
+          <Logo />
+          <ThemeSwitcher />
+        </div>
+      </div>
+    </header>
+  );
+}
