66ed895617c4bf9a0a7482ffcc7f9d83d4750862
baselelsrogy <baselhany88@gmail.com>
2026-05-30 02:24:40 +0300

    add logo and switch theme to header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from './components/Header';
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-    </>
+    <main className="font-inter dark:text-gray-100">
+      <Header />
+    </main>
   );
 }
 
